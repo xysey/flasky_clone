2e665dc32ac1bd6d37a8337243510215b80b610f
xysey <xysey6@gmail.com>
2019-07-31 10:13:24 +0800

    Templatesgit add *!

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
-@app.route('/user/<name')
+@app.route('/user/<name>')
 def user(name):
-    return f'<h1>Hello World!</h1>'
+    return render_template('user.html', name = name)
+
+if __name__ == "__main__":
+    app.env = 'development'
+    app.debug = 1
+    app.run()
